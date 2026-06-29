da7020414638f993cd2e935d34f13a552a78bb67
Ubuntu <ubuntu@ip-10-0-8-105.us-east-2.compute.internal>
2026-06-29 04:54:51 +0000

    modificando las IPs del front en '.env'

--- vite.config.js
+++ vite.config.js
@@ -21,15 +21,15 @@ export default defineConfig(({ mode }) => {
           secure: false,
         }
       }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Si el módulo viene de node_modules, lo separa en un chunk independiente
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // Si el módulo viene de node_modules, lo separa en un chunk independiente
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
           }
         }
       }
